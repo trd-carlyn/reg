75eedc7b1d4e4dc2dc14f87730627132bb921650
trd.Carlyn Carmel Orio <trd.carlyncarmel@gmail.com>
2017-09-08 18:11:30 +0800

    [add]css and images

---
+++ index.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> </title>
+	<link rel="stylesheet" href="/css/style.css" type="text/css">
+</head>
+<body>
+	<div class="container">
+		<div class="main-header">
+			<div class="mast-header">
+				<div class="header-logo">
+					<img src ="../img/regrowth.png" alt="regrowth">
+				</div>
+				<div class ="reserve-form">
+					<img src ="../img/reserve-form.png" alt="reserve-form">
+				</div>
+				<div class="reserve-call">
+					<img src ="../img/reserve-call.png" alt="reserve-call">
+				</div>
+				<div class="map">
+					<div class="reserve-map">
+						<p> >MAP </p>
+					</div>
+				</div>
+				<div class="navigation">
+					<div class="menu-links">
+						<ul>
+							<li> <a href="#">HOME</a> </li>
+							<li><a href="#"> マイクロバブルにて </a></li>
+							<li><a href="#"> 施術事例  </a></li>
+							<li><a href="#"> 施術の流れ </a></li>
+							<li><a href="#"> 料金  </a></li>
+							<li><a href="#"> 店舗案内 </a></li>
+						</ul>
+					</div>
+					<div class="social-links">
+						<ul>
+							<li> <a href =""> <img src ="/img/fb.png" alt="fb"> </a></li>
+							<li> <a href =""> <img src ="/img/amebo.png" alt="amebo"> </a></li>
+							<li> <a href =""> <img src ="/img/insta.png" alt="insta"> </a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="header-thummbnail">
+				<div class ="header-top-thumbnail">
+					<img src="/img/top-header.png">
+				 </div>
+			</div>
+		</div>
+
+		<div class="content">
+			<div class ="main-content">
+				<div class="first-content">
+					<div class="article">
+						<div class="article feature">
+							<img src="/img/content.png">
+						</div>
+						<div class="features">
+							<h2 class ="mb-feature">
+								<span> マイクロバブル
+									<span> つの特徴 </span>
+								 </span>
+							</h2>
+						</div>
+						<div class="featured">
+							<div class = "featured-image">
+								<img src="/img/top1.png">
+								<h3> バツグンの洗浄効果</h3>
+								<p> 天然酵素と植物エキスが入ったマ<br>イクロバブルを10分かけ流すだ<br>けで、毛先から毛穴の中まで汚<br>
+								れや余分な皮脂を取り除きます。</p>
+							</div>
+							<div class = "featured-image">
+								<img src="/img/top2.png">
+								<h3> こすらず洗える</h3>
+								<p> 超微細な泡洗浄で頭皮をこする<br>ことなく洗うことにより、髪の細<br>りやハリやコシの衰えをカバ
+								ーし、<br> 頭皮や毛髪へのアンチエイジング<br>効果も期待できます。</p>
+							</div>
+							<div class = "featured-image">
+								<img src="/img/top3.png">
+								<h3> においスッキリ</h3>
+								<p>マイクロバブルの超微細泡洗浄<br>が、頭皮の汚れなどからくる気に<br>なる臭いもスッキリ解消します。</p>
+							</div>
+							<div class = "featured-image">
+								<img src="/img/top4.png">
+								<h3> リラクゼーション効果</h3>
+								<p>20ミクロンの安定した泡の質、<br>大量の泡のはじける刺激で血行<br>や血流を促進し、空気のビタミン<br>と言われるマイナスイオンの発生<br>により安息作用を促します。</p>
+							</div>
+						</div>
+							<div class="top-link">
+								<a href="">
+									<img src="/img/top-link.png">
+								</a>
+							</div>
+						<div class="hair-growth">
+							<div class="hair-growth-feature">
+								<div class="feature2">
+									<h2 class="hg-feature">
+										<span> 発毛の事例</span>
+									</h2>
+								</div>
+								<div class="title-feature">
+									<p> マイクロバブルを使うことで、なぜ発毛するの？ </p>
+								</div>
+								<div class="hairgrowth-feature">
+									<img src="/img/top5.png">
+									<p> 地肌が露出した頭皮、よく見ると油脂で毛穴がふさがっています。実は少しでもうぶ毛のような状態であれば、それを育てることは困難なことではありません。これから生えてくる髪の毛が育ちやすい環境にするために、<span class="red"> マイクロバブルで毛穴の汚れ</span> を取り除いていきます。 </p>
+								</div>
+								<div class="title-feature2">
+									<p> 週1回の施術で、1週間ごとの変化を見てみます </p>
+								</div>
+								<div class="hairgrowth-feature">
+									<img src="/img/top6.png">
+									<p> 髪の毛の細く抜けやすい頭髪を、太く抜けにくい毛髪にしていくため、定期的な施術を開始しました。個人差はありますが、<span class="red">髪の毛が徐々に太くなり、毛髪にハリが出てくる</span> など変化が見られました。 </p>
+								</div>
+								<div class="hairgrowth-feature">
+									<div class="link">
+										<div class="top-link2">
+											<img src="/img/link2.png">
+										</div>
+										<div class="top-link3">
+											<img src="/img/link3.png">
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="campaign">
+							<a href=""> <img src="/img/image4.png"></a>
+						</div>
+						<div class="latest-information"> 
+							<h2 class="information-title">
+								<span> 最新情報</span>
+							 </h2>
+							 <div class="latest-information-features">
+							<div class="info-1">
+								<div class="box-1">
+									<img src="/img/box1.png">
+								</div>
+								<div class="information">
+									<div class="info">
+										<div class="info-date">
+											<p> 00/00/00</p>
+										</div>
+										<div class="info-category">
+											<img src="/img/category1.png">
+										</div>
+									</div>
+									<div class="article-title">
+										<p>記事タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル </p>
+									</div>
+								</div>
+							</div>
+							<div class="info-1">
+								<div class="box-2">
+									<img src="/img/box1.png">
+								</div>
+								<div class="information">
+									<div class="info">
+										<div class="info-date">
+											<p> 00/00/00</p>
+										</div>
+										<div class="info-category">
+											<img src="/img/category2.png">
+										</div>
+									</div>
+									<div class="article-title">
+										<p>記事タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル </p>
+									</div>
+								</div>
+							</div>
+							<div class="info-1">
+								<div class="box-3">
+									<img src="/img/box1.png">
+								</div>
+								<div class="information">
+									<div class="info">
+										<div class="info-date">
+											<p> 00/00/00</p>
+										</div>
+										<div class="info-category">
+											<img src="/img/category3.png">
+										</div>
+									</div>
+									<div class="article-title">
+										<p>記事タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル </p>
+									</div>
+								</div>
+							</div>
+						</div>
+						</div>
+						
+						<div class="top-link4">
+							<a href="">
+								<img src="/img/link4.png">
+							</a>
+						</div>
+						<div class="treatment"> 
+							<h2 class="treatment-title">
+								<span> 施術事例 </span>
+							</h2>
+							<div class="slide-images">
+								<img src="/img/box1.png">
+								<img src="/img/box1.png">
+								<img src="/img/box1.png">
+								<img src="/img/box1.png">
+								<img src="/img/box1.png">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="footer">
+			<div class="nav-footer">
+				<ul>
+					<li> <a href=""> HOME </a> </li>
+					<li> <a href="">  マイクロバブルにて</a></li>
+					<li> <a href=""> </a> </li>
+					<li> <a href="">  施術の流れ</a> </li>
+					<li> <a href=""> 料金 </a> </li>
+					<li> <a href=""> 店舗案内</a> </li>
+					<li> <a href=""> アクゼス MAP</a> </li>
+					<li> <a href=""> 運営会社</a> </li>
+					<li> <a href="">  ご予約</a> </li>
+				</ul>
+			</div>
+			<div class="mastfooter">
+				<div class="header-logo">
+					<img src ="../img/bottom-logo.png" alt="regrowth">
+				</div>
+						<ul>
+							<li> <a href =""> <img src ="/img/fb2.png" alt="fb"> </a></li>
+							<li> <a href =""> <img src ="/img/amebo2.png" alt="amebo"> </a></li>
+							<li> <a href =""> <img src ="/img/insta2.png" alt="insta"> </a></li>
+						</ul>
+				<div class="call-bottom">
+					<a href=""> <img src="../img/reserve-call.png"> </a>
+				</div>
+				<div class="form-bottom">
+					<a href=""> <img src="../img/reserve-form.png"> </a>
+				</div>
+				<hr>
+				<div class="contact-info">
+				<p>	〒150-0013
+				<p>	東京都渋谷区恵比寿4-4-2
+				<p>	クレスト恵比寿506号
+				<p>	JR恵比寿駅東口から徒歩2分
+				<p>	TEL	03-6450-4120
+				<p>	営業時間	平日　12:00~21:00
+				<p>	 	土/日/祝日10:00~19:00
+				<p>	定休日	月曜、火曜
+				<p>	（祝日の場合は翌平日がお休みになります）
+				</div>
+				<div class="calendar">
+					<p> 営業日カレンダー</p>
+				</div>
+				<div class="copyright">
+					<p> Copyright(C) 2017 ReGrowth.All rights reserved.
+				</div>
+			</div>
+		</div>
+
+	</div>
+</body>
+</html>
